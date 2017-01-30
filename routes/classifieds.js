ca3b2e805f375e4cafeb834f9c93ee85d4c1a481
KatieAJenkins <kind2karma@gmail.com>
2017-01-30 09:57:45 -0700

    get route fully functional and passing test

--- routes/classifieds.js
+++ routes/classifieds.js
@@ -7,16 +7,16 @@ const router = express.Router();
 const knex = require('../knex');
 
 router.get ('/' , (req, res, next) => {
-  console.log('get connected!');
-  //
-  // knex('classifieds')
-  //
-  // .then((results) => {
-  //   res.send(results);
-  // })
-  // .catch((err) => {
-  //   next(err);
-  // });
+  // console.log('get route connected!');
+
+  knex('classifieds')
+    .select ('id', 'title' , 'description' , 'price' , 'item_image')
+  .then((results) => {
+    res.send(results);
+  })
+  .catch((err) => {
+    next(err);
+  });
 });
 
 // router.get('/:id' , (req, res, next) => {
