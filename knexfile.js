0ac3b3efb21862ef580a7f05583d7dc1fccaeea2
KatieAJenkins <kind2karma@gmail.com>
2017-01-30 09:39:13 -0700

    migration file complete

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,17 @@
-// Update with your config settings.
+'use strict';
 
 module.exports = {
 
-  development: { },
-  test: { },
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/classifieds_dev'
+  },
+
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/classifieds_test'
+   },
+
   production: { },
 
 };
