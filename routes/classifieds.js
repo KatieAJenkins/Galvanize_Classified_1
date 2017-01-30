7f8c3c377954524d32a72d816ea5c5a44a6dee4e
KatieAJenkins <kind2karma@gmail.com>
2017-01-30 09:55:22 -0700

    get route connected

--- routes/classifieds.js
+++ routes/classifieds.js
@@ -1,10 +1,36 @@
-
 'use strict';
 
 const express = require('express');
 
 const router = express.Router();
 
-// YOUR CODE HERE
+const knex = require('../knex');
+
+router.get ('/' , (req, res, next) => {
+  console.log('get connected!');
+  //
+  // knex('classifieds')
+  //
+  // .then((results) => {
+  //   res.send(results);
+  // })
+  // .catch((err) => {
+  //   next(err);
+  // });
+});
+
+// router.get('/:id' , (req, res, next) => {
+//   console.log('get by id connected');
+//   //
+//   // knex('classifieds')
+//   //
+//   // .then((results) => {
+//   //   res.send(results);
+//   // })
+//   // .catch((err) => {
+//   //   next(err);
+//   // });
+// });
+
 
 module.exports = router;
