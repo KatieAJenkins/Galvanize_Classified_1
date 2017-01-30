d34358ac9dd5a332864c5f6777e68a412d5767ba
KatieAJenkins <kind2karma@gmail.com>
2017-01-30 09:59:58 -0700

    get by id route fully functional and passing test

--- routes/classifieds.js
+++ routes/classifieds.js
@@ -20,16 +20,20 @@ router.get ('/' , (req, res, next) => {
 });
 
 router.get('/:id' , (req, res, next) => {
-  console.log('get by id connected');
-  //
-  // knex('classifieds')
-  //
-  // .then((results) => {
-  //   res.send(results);
-  // })
-  // .catch((err) => {
-  //   next(err);
-  // });
+  // console.log('get by id connected');
+
+  const id = req.params.id;
+
+  knex('classifieds')
+    .select('id' , 'title' , 'description' , 'price' , 'item_image')
+    .where('id' , id)
+
+  .then((results) => {
+    res.send(results[0]);
+  })
+  .catch((err) => {
+    next(err);
+  });
 });
 
 
